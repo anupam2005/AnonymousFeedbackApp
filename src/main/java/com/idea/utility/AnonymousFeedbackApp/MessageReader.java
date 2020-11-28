9d03bfea4cf6721a912120d0b88fe2f3012b4a75
absis777@gmail.com <absis777@gmail.com>
2020-11-29 01:54:23 +0530

    Configuring Junit-5.

--- src/main/java/com/idea/utility/AnonymousFeedbackApp/MessageReader.java
+++ src/main/java/com/idea/utility/AnonymousFeedbackApp/MessageReader.java
@@ -1,12 +1,12 @@
 package com.idea.utility.AnonymousFeedbackApp;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
 
 import javax.jms.JMSException;
-import javax.jms.Message;
+//import javax.jms.Message;
 import javax.jms.Queue;
 
 import org.apache.activemq.command.ActiveMQTextMessage;
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.common.collect.Lists;
+//import com.google.common.collect.Lists;
 
 import javax.jms.QueueBrowser;
 import javax.jms.Session;
